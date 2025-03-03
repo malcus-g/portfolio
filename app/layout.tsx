d9cb2eec7adf8b8db117957d745536a0cfb939b1
Garrett <garrettcouch97@gmail.com>
2025-03-03 12:27:37 -0500

    add background colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${ubuntuSans.variable} antialiased`}>
+        className={`${ubuntuSans.variable} antialiased 
+                  bg-slate-50 text-slate-900 relative`}>
+        <div className="bg-[#e8cbfbb9] absolute -z-10 top-[-1rem] right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dddafeb1] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         {children}
       </body>
     </html>
