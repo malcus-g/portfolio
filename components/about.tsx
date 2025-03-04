bfe51ab72b7d673bec67242064616cbf354cf32c
Garrett <garrettcouch97@gmail.com>
2025-03-04 12:54:43 -0500

    add Projects section and update About component; refine section styles

--- components/about.tsx
+++ components/about.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function About() {
   return (
     <motion.section
-    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    className="max-w-[45rem] text-center leading-8"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{
--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 export default function SectionDivider() {
   return (
     <motion.div 
-    className="bg-slate-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+    className="bg-slate-200 my-24 h-1 w-16 rounded-full hidden sm:block"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
     transition= {{
