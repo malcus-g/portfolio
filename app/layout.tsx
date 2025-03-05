3385390faac08b171eb5f4dc51c7feed937b37f9
Garrett <garrettcouch97@gmail.com>
2025-03-05 15:08:33 -0500

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const ubuntuSans = Ubuntu_Sans({
   variable: "--font-ubuntu-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster position="bottom-center" />
+          <Footer />
         </ActiveSectionContextProvider>
       </body>
     </html>
